1dce16308e1e91e8bbf09478b7270f6b765da916
Ian Young <ian@iangreenleaf.com>
2023-02-11 16:13:19 -0600

    Use specified domain when sending emails

--- lib/pict_web/emails/user_email.ex
+++ lib/pict_web/emails/user_email.ex
@@ -1,10 +1,15 @@
 defmodule PictWeb.Emails.UserEmail do
   use Phoenix.Swoosh, view: PictWeb.EmailView, layout: {PictWeb.LayoutView, :email}
 
+  defp default_from(mail) do
+    host = System.get_env("PHX_HOST") || "example.com"
+    from(mail, {"Telephone Pictionary", "pict@#{host}"})
+  end
+
   def game_ready(game) do
     new()
     |> to(game.owner)
-    |> from({"Telephone Pictionary", "pict@example.com"})
+    |> default_from()
     |> subject("Your Telephone Pictionary game is ready: #{game.name}")
     |> render_body("game_ready.html", %{game: game})
   end
@@ -12,7 +17,7 @@ defmodule PictWeb.Emails.UserEmail do
   def submission_ready(submission) do
     new()
     |> to(submission.player)
-    |> from({"Telephone Pictionary", "pict@example.com"})
+    |> default_from()
     |> subject("Take your turn in Telephone Pictionary")
     |> render_body("submission_ready.html", %{submission: submission})
   end
@@ -20,7 +25,7 @@ defmodule PictWeb.Emails.UserEmail do
   def prompt_ready(submission, owner) do
     new()
     |> to(submission.player)
-    |> from({"Telephone Pictionary", "pict@example.com"})
+    |> default_from()
     |> subject("You've been invited to play Telephone Pictionary")
     |> render_body("prompt_ready.html", %{submission: submission, owner: owner})
   end
