10339bc32696c7e5c955d250631179d9bbce5582
Ian Young <ian@iangreenleaf.com>
2023-03-03 22:33:18 -0600

    Show prompt name on submission page

--- lib/pict_web/live/submission_live/show.ex
+++ lib/pict_web/live/submission_live/show.ex
@@ -15,12 +15,14 @@ defmodule PictWeb.SubmissionLive.Show do
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
     submission = Prompts.get_submission!(id)
+    %{starter_name: starter_name, total: total} = Prompts.get_submission_name_details(submission)
 
     {
       :noreply,
       socket
       |> assign(:page_title, page_title(socket.assigns.live_action))
       |> assign(:submission, submission)
+      |> assign(:name, submission_name(submission, starter_name, total))
       |> assign(:next_submissions, Prompts.other_undone_submissions(submission))
       |> assign(:clue, Prompts.get_clue_for(submission))
     }
@@ -29,6 +31,9 @@ defmodule PictWeb.SubmissionLive.Show do
   def drawing_url(submission, size \\ :large) do
     Pict.Drawing.url({submission.drawing, submission}, size)
   end
+
+  defp submission_name(submission, starter_name, total), do:
+    "#{starter_name} #{submission.order + 1}/#{total}"
 
   defp page_title(:show), do: "Show Submission"
   defp page_title(:edit), do: "Edit Submission"
